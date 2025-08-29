34be9fab8d0c4b4a4d5fea6055cc1f4e9f877dbc
namansharma28 <namansharma.web@gmail.com>
2025-08-29 15:26:31 +0530

    changed link

--- vite.config.js
+++ vite.config.js
@@ -34,5 +34,5 @@ const copyVideoPlugin = () => {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), copyVideoPlugin()],
-  base: "/Parametric-Modelling-dept-Nexura",
+  base: "/",
 });
